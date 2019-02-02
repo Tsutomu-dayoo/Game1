26472f7a55babdb9340034bd39bed654b5704ed1
Tsutomu Yamaguchi <t.yamaguchi.air@outlook.jp>
2019-02-03 04:09:18 +0900

    Initial Commit

---
+++ Game1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game1
+//
+//  Created by 山口勉 on 2019/02/03.
+//  Copyright © 2019年 山口勉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
